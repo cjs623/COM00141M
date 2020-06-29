bb4d46c3f521e0ecadaae7ee7d6c23d87e2fa7df
cjs623 <67607023+cjs623@users.noreply.github.com>
2020-06-29 22:05:02 +0100

    upload

---
+++ Week1/Lab 2/Exercise 1/TestExciting.java
@@ -0,0 +1,20 @@
+
+public class TestExciting {
+	
+ public static void main (String args[]) {
+	 
+	 //after 1a) create an Exciting object, and display the details for the object
+	 
+	 Exciting ex = new Exciting();
+	 
+	 ex.displayDetails();
+	 
+	//after 1b) create an Exciting object, and display the details for the object
+	 
+	 //ex.
+	 
+	 
+	 
+ }
+
+}
