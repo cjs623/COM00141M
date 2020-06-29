ba7cb99b791012f78cea1d16a167b635c38e99fe
cjs623 <67607023+cjs623@users.noreply.github.com>
2020-06-29 21:39:07 +0100

    Upload

---
+++ Week1/Lab 1/Hello.java
@@ -0,0 +1,25 @@
+/**
+ *
+ * Class Hello
+ *  Hello world program
+ */
+class Hello {
+    public static void main(String[] args) {
+            System.out.println("Hello, welcome to the University of York");
+            //put new code here
+            String Header	= 	"My Initials\n" + 
+                                "------------------\n";
+            String initials = 	"	*********	*********	*********\n" + 
+                                "	*       	    *    	*		 \n" +
+                                "	*       	    *   	*        \n" + 
+                                "	*       	    *   	*        \n" + 
+                                "	*       	    *    	*********\n" + 
+                                "	*       	    *		        *\n" + 
+                                "	*       	    *		        *\n" + 
+                                "	*       	    *		        *\n" + 
+                                "	*********	*****   	*********";
+                                
+            System.out.println(Header);
+            System.out.println(initials);
+    }
+}
