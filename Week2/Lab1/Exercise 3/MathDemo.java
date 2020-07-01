9a6764d95853a407042ab7ea24c07ba64525191d
cjs623 <67607023+cjs623@users.noreply.github.com>
2020-07-01 20:14:33 +0100

    Upload

--- Week2/Lab1/Exercise 3/MathDemo.java
+++ Week2/Lab1/Exercise 3/MathDemo.java
@@ -7,6 +7,10 @@ import java.util.*;
 public class MathDemo
 {
 	
+	int firstInt=0;
+	int secondInt=0;
+	int thirdInt=0;
+	
 	public double squareRoot(double rootIn)
 	{
 	 return  Math.sqrt(rootIn);
@@ -17,6 +21,56 @@ public class MathDemo
 	   int score;
 	   score = (int) (Math.random() * 6) + 1;
 	   System.out.println("You rolled a " + score);
+	}
+	
+	public int generateLottoNumber() {
+		return (int) (Math.random() * 49) + 1;		
+	}
+	
+	public int getHighestInt(int firstIntIn,int secondIntIn,int thirdIntIn) {
+		if (firstIntIn >= secondIntIn && firstIntIn >= thirdIntIn) {
+			return firstIntIn;
+		}else if (secondIntIn >= thirdIntIn) {
+			return secondIntIn;
+		}else {
+			return thirdIntIn;
+		}			
+	}
+	
+	public void bestOfThree() {
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter first number and press return ");
+		firstInt = sc.nextInt();
+		System.out.println("First number set to: " + String.valueOf(firstInt));
+		
+		System.out.println("Enter second number and press return ");
+		secondInt = sc.nextInt();
+		System.out.println("Second number set to: " + String.valueOf(secondInt));
+		
+		System.out.println("Enter third number and press return ");
+		thirdInt = sc.nextInt();
+		System.out.println("Third number set to: " + String.valueOf(thirdInt));		
+		
+		
+		System.out.println("Highest number provided: " + String.valueOf(getHighestInt(firstInt,secondInt,thirdInt)) + "\n\n");
+		
+		
+	}
+	
+	public void displayHighestRandom() {
+		
+		firstInt = generateLottoNumber();
+		System.out.println("First number set to: " + String.valueOf(firstInt));
+		
+		secondInt = generateLottoNumber();
+		System.out.println("Second number set to: " + String.valueOf(secondInt));
+		
+		thirdInt = generateLottoNumber();
+		System.out.println("Third number set to: " + String.valueOf(thirdInt));	
+		
+		System.out.println("Highest number provided: " + String.valueOf(getHighestInt(firstInt,secondInt,thirdInt)) + "\n\n");		
+		
 	}
 	
 	public void findThePower()
