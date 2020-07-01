583b078433e8bf8a8e1007908c883950af184fe1
cjs623 <67607023+cjs623@users.noreply.github.com>
2020-07-01 20:20:19 +0100

    Upload

--- Week2/Lab1/Exercise 4/Car.java
+++ Week2/Lab1/Exercise 4/Car.java
@@ -6,10 +6,25 @@ class Car
     
     //Methods to set car details below here
     
+    public void setReg(String regIn) {
+    	registration = regIn;
+    }
     
+    public void setMake(String makeIn) {
+    	make = makeIn;
+    }
+    
+    public void setPrice(double priceIn) {
+    	price = priceIn;
+    }    
     
     //displayCarDetails()
     
+    public void displayCarDetails(){
+		System.out.println("Registration set to: " + getRegistration());
+		System.out.println("Make set to: " + getMake());
+		System.out.println("Price set to: " + getPrice());
+    }
     
     
     //Try out these 'getter' methods after you have set info.
--- Week2/Lab1/Exercise 4/TestCar.java
+++ Week2/Lab1/Exercise 4/TestCar.java
@@ -3,7 +3,11 @@ public class TestCar {
 
 	public static void main(String[] args) {
 		//Create Car object and text car methods 
-
+		Car ca = new Car();
+		ca.setReg("M212 EMW");
+		ca.setMake("Ford Escort");
+		ca.setPrice(998.98);
+		ca.displayCarDetails();
 	}
 
 }
