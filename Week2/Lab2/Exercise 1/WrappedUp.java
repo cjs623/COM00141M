27be30b7f0914922aad2a90cfbff4cbaa3554a88
cjs623 <67607023+cjs623@users.noreply.github.com>
2020-07-07 18:15:38 +0100

    Upload

--- Week2/Lab2/Exercise 1/WrappedUp.java
+++ Week2/Lab2/Exercise 1/WrappedUp.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 /**
  * Favourite class to test a string object and it's methods.
  * 
@@ -12,15 +12,43 @@ public class WrappedUp
 	{
 	   //Create a string object with your favourite animal, car ..whatever
 	   //e.g String favMotorBikeManufacturer = "bmw"
+		
+		String favCarModel = new String("Civic");
+		int favCarModelStrLen = favCarModel.length();
+		String favCarModelFstChar = favCarModel.substring(0,1);
+		String enteredAns = "";
+		boolean guessedCorrect = false;
+		
+		
 	   
 	   /* Now prompt for a guess - using the Scanner class
 	   ** Using String methods, you should give the length 
 	   ** and first letter of your favourite thing in the prompt.
 	   */
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.printf("Item to guess is a car model which is %s letters long and begins with %s \n",favCarModelStrLen,favCarModelFstChar);		
+		
+		  // Now test if the guess was correct - ignore case 		
+		
+		do {
+		
+			System.out.println("Enter your guess: ");
+			enteredAns = sc.next();		
+			
+			if (favCarModel.equalsIgnoreCase(enteredAns)){
+				guessedCorrect = true;
+				System.out.println("You guessed correctly, congrats!");
+			}else {				
+				System.out.println("Sorry thats wrong, try again :(");				
+			}
+		}while (!guessedCorrect);
+		
+		sc.close();	
 	  
 	  
-	  
-	  // Now test if the guess was correct - ignore case 
+
 	 
 	
 	
@@ -28,8 +56,9 @@ public class WrappedUp
 	}//end guessing
 
 
-	public static void main(String args[]) {
-		//Create WrappedUp object and test the methods
+	
+	public static void main(String args[]) { //Create WrappedUp object and test the methods
 		
 	}
+	
 }
---
+++ Week2/Lab2/Exercise 1/WrappedUpTest.java
@@ -0,0 +1,12 @@
+
+public class WrappedUpTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		WrappedUp wu = new WrappedUp();
+		wu.guessMyFavouriteThing();
+		
+	}
+
+}
