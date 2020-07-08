2c97e5a86b9c6cec7760185c2c8d00c4b7bd0d4d
cjs623 <67607023+cjs623@users.noreply.github.com>
2020-07-08 17:36:09 +0100

    Upload

--- Week3/Lab2/Exercise5/src/ChangingFace.java
+++ Week3/Lab2/Exercise5/src/ChangingFace.java
@@ -1,5 +1,7 @@
-import javafx.application.Application; 
-import javafx.scene.Group; 
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene; 
 import javafx.scene.control.Button; 
 import javafx.scene.layout.Background; 
@@ -8,13 +10,55 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color; 
 import javafx.scene.shape.Arc; 
 import javafx.scene.shape.ArcType; 
-import javafx.scene.shape.Circle; 
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.scene.text.Font; 
 import javafx.scene.text.Text; 
 import javafx.stage.Stage; 
 import javafx.geometry.Pos;
 
 public class ChangingFace extends Application {
+	
+	boolean[] smilePresent = new boolean[3];
+	 // create and configure a smiling mouth (this is how it will start)    
+	 Arc smileMouth = new Arc(125, 150, 45, 35, 0, -180);        
+	 Arc frownMouth = new Arc(125, 150, 45, 35, 0, 180);          
+	 Line thinkMouth = new Line(50,100,200,100);
+		
+	 
+	 public ChangingFace() {
+		 
+		 smileMouth.setFill(Color.YELLOW);         
+		 smileMouth.setStroke(Color.BLUE);         
+		 smileMouth.setType(ArcType.OPEN);
+		 
+		 frownMouth.setFill(Color.YELLOW);         
+		 frownMouth.setStroke(Color.BLUE);         
+		 frownMouth.setType(ArcType.OPEN); 	
+		 
+		 thinkMouth.setFill(Color.YELLOW);  		 
+		 thinkMouth.setStroke(Color.BLUE);			 
+		 
+	 }
+	
+	
+	public void printout(String type,double len,double a, double b,ObservableList<Node> ol) {
+		System.out.printf("type: %s, len: %s, x: %s, y: %s\n",type,len,a,b);
+		for(int i =0;i< ol.size();i++) {
+			System.out.println(ol.get(i));
+		}
+	}
+	
+	public boolean[] findSmiles(Group group) {
+		boolean[] smilePresentOut = new boolean[3];
+		
+		smilePresentOut[0] = group.getChildren().contains(smileMouth);
+		smilePresentOut[1] = group.getChildren().contains(thinkMouth);
+		smilePresentOut[2] = group.getChildren().contains(frownMouth);
+		
+		return smilePresentOut;
+		
+	}
 	
 	 @Override     
 	 public void start(Stage stage)     
@@ -34,11 +78,7 @@ public class ChangingFace extends Application {
 		 leftEye.setFill(Color.YELLOW);         
 		 leftEye.setStroke(Color.BLUE);   
 		 
-		 // create and configure a smiling mouth (this is how it will start)    
-		 Arc mouth = new Arc(125, 150, 45, 35, 0, -180);          
-		 mouth.setFill(Color.YELLOW);         
-		 mouth.setStroke(Color.BLUE);         
-		 mouth.setType(ArcType.OPEN); 
+ 
 		 
 		 // create and configure the text        
 		 Text caption = new Text(68, 240, "Changing Face");         
@@ -46,20 +86,23 @@ public class ChangingFace extends Application {
 		 caption.setFont(Font.font ("Verdana", 15));
 		 
 		 // create a group that holds all the features           
-		 Group group = new Group(face, rightEye, leftEye, mouth,  caption); 
+		 Group group = new Group(face, rightEye, leftEye, smileMouth,  caption); 
 		 
 		 // create a button that will make the face smile         
 		 Button smileButton = new Button("Smile"); 
 		 
 		 // create a button that will make the face frown         
 		 Button frownButton = new Button("Frown"); 
+		 
+		 // create a button that will make the face think         
+		 Button thinkButton = new Button("Think");		 
 	 
 	     // create and configure a horizontal container to hold the buttons    
 		 HBox buttonBox = new HBox(10);         
 		 buttonBox.setAlignment(Pos.CENTER); 
 		 
 		 //add the buttons to the horizontal container         
-		 buttonBox.getChildren().addAll(smileButton, frownButton);
+		 buttonBox.getChildren().addAll(smileButton,thinkButton, frownButton);
 		 
 		 // create and configure a vertical container to hold the button box and the face group         
 		 VBox root = new VBox(10); 
@@ -70,13 +113,68 @@ public class ChangingFace extends Application {
 		 root.getChildren().addAll(buttonBox, group);
 		 
 		 // create and configure a new scene         
-		 Scene scene = new Scene(root, 250, 275, Color.YELLOW);
+		 Scene scene = new Scene(root, 250, 275, Color.YELLOW);		
 		 
 		 // supply the code that is executed when the smile button is pressed  
-		 smileButton.setOnAction(e -> mouth.setLength(-180));
+		 smileButton.setOnAction(e -> 	{
+			 							smilePresent = findSmiles(group);			 							
+										
+										if(!smilePresent[0]) {
+											if(smilePresent[1]) {
+												group.getChildren().remove(thinkMouth);
+												System.out.println("Removing Think Mouth");
+											}
+											if(smilePresent[2]) {
+												group.getChildren().remove(frownMouth);
+												System.out.println("Removing Frown Mouth");
+											}											
+											
+											group.getChildren().add(smileMouth);
+											System.out.println("Adding Smile Mouth");
+										}
+		 								}
+			 					);
+		 
+		 // supply the code that is executed when the think button is pressed  
+		 thinkButton.setOnAction(e -> 	{
+			 							smilePresent = findSmiles(group);			 							
+										
+										if(!smilePresent[1]) {
+											if(smilePresent[0]) {
+												group.getChildren().remove(smileMouth);	
+												System.out.println("Removing Smile Mouth");
+											}
+											if(smilePresent[2]) {
+												group.getChildren().remove(frownMouth);
+												System.out.println("Removing Frown Mouth");
+											}											
+											
+											group.getChildren().add(thinkMouth);
+											System.out.println("Adding Think Mouth");
+										}										
+										}
+				 				);	
 		 
 		 // supply the code that is executed when the frown button is pressed   
-		 frownButton.setOnAction(e -> mouth.setLength(180)); 
+		 frownButton.setOnAction(e -> 	{		 			 							
+			 							smilePresent = findSmiles(group);
+										if(!smilePresent[2]) {
+											if(smilePresent[0]) {
+												group.getChildren().remove(smileMouth);	
+												System.out.println("Removing Smile Mouth");
+											}
+											if(smilePresent[1]) {
+												group.getChildren().remove(thinkMouth);
+												System.out.println("Removing Think Mouth");
+											}											
+											
+											group.getChildren().add(frownMouth);
+											System.out.println("Adding Frown Mouth");
+										}		 							
+			 							
+										
+										}
+				 				);		 
 		 
 		 // add the scene to the stage, then set the title        
 		 stage.setScene(scene);         
